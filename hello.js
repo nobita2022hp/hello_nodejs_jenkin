17a1c242e6ee845329428a8adfc3c03165a5ecce
nobita2022hp <100983927+nobita2022hp@users.noreply.github.com>
2022-04-03 21:48:36 +0700

    Update hello.js
    change demo

--- hello.js
+++ hello.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 app.get('/contact', function (req, res) {
     res.send('Hello Node');
+    console.log('Run success');
 });
 
 app.listen(3000, function () {
